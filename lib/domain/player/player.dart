3f654e0a59dd8bc5d073458e34d2df39c58c76d3
Loïc Kervran <loker29@hotmail.fr>
2023-08-20 20:26:29 +0200

    feat: add current player, error handling
    fix: fix diagonal win typo
    test: add a test to check the win on a single move

--- lib/domain/player/player.dart
+++ lib/domain/player/player.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
+
 class Player {
-  const Player(this.id);
+  const Player({required this.id, required this.icon});
 
   final int id;
+  final IconData icon;
 }
--- lib/domain/tic_tac_toe/tic_tac_toe.dart
+++ lib/domain/tic_tac_toe/tic_tac_toe.dart
@@ -1,42 +1,48 @@
+import 'package:flutter/material.dart';
+
 import '../player/player.dart';
 import 'tic_tac_toe.interface.dart';
 
 class TicTacToe extends AbstractTicTacToe {
   TicTacToe();
 
-  final Board board =
-      List<List<Player?>>.filled(3, List<Player?>.filled(3, null));
-  final Player player1 = const Player(1);
-  final Player player2 = const Player(2);
+  Board board = <List<Player?>>[
+    <Player?>[null, null, null],
+    <Player?>[null, null, null],
+    <Player?>[null, null, null],
+  ];
+  final Player player1 = const Player(id: 1, icon: Icons.circle);
+  final Player player2 = const Player(id: 2, icon: Icons.close_rounded);
+  late Player currentPlayer = player1;
 
   @override
   Player? findWinner() {
-    // Check for vertical and horizontal wins
-    for (int x = 0; x < board.length - 1; x++) {
-      if (board[x][0] != null &&
-          board[x][0] == board[x][1] &&
-          board[x][0] == board[x][2]) {
-        return board[x][0];
+    // Check for horizontal and vertical wins
+    for (int i = 0; i < board.length; i++) {
+      if (board[i][0] != null &&
+          board[i][0]?.id == board[i][1]?.id &&
+          board[i][0]?.id == board[i][2]?.id) {
+        return board[i][0];
       }
 
-      if (board[0][x] != null &&
-          board[0][x] == board[1][x] &&
-          board[0][x] == board[2][x]) {
-        return board[0][x];
+      if (board[0][i] != null &&
+          board[0][i]?.id == board[1][i]?.id &&
+          board[0][i]?.id == board[2][i]?.id) {
+        return board[0][i];
       }
     }
 
     // Check top-left to bottom-right diagonal win
     if (board[0][0] != null &&
-        board[1][1] == board[0][0] &&
-        board[2][2] == board[0][0]) {
+        board[1][1]?.id == board[0][0]?.id &&
+        board[2][2]?.id == board[0][0]?.id) {
       return board[0][0];
     }
 
     // Check top-right to bottom-left diagonal win
     if (board[0][2] != null &&
-        board[1][1] == board[0][0] &&
-        board[2][0] == board[0][0]) {
+        board[1][1]?.id == board[0][2]?.id &&
+        board[2][0]?.id == board[0][2]?.id) {
       return board[0][2];
     }
 
@@ -65,15 +71,22 @@ class TicTacToe extends AbstractTicTacToe {
 
   @override
   void play(Player player, int x, int y) {
+    // Forbidden move, cell has already been played
+    if (board[x][y] != null) {
+      throw Error();
+    }
+
     board[x][y] = player;
 
     final Player? winner = findWinner();
     if (winner != null) {
       // Player won
+      return;
     }
 
     if (isBoardFull()) {
       // Emit draw
+      return;
     }
 
     return;
